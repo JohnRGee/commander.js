c0e66635305d94549dddd0e3c9d5d4cdf16ede8a
John Gee <john@ruru.gen.nz>
2020-12-26 21:27:08 +1300

    Add some negate tests

--- tests/command.implies.test.js
+++ tests/command.implies.test.js
@@ -54,3 +54,26 @@ test('when x with optional value implies y and -xValue then y is true', () => {
   program.parse(['-xValue'], { from: 'user' });
   expect(program.opts().y).toBe(true);
 });
+
+test('when x implies y and --no-x then y is false', () => {
+  const program = new commander.Command();
+  program
+    .option('--no-x')
+    .option('-y')
+    .implies('x', 'y');
+
+  program.parse(['--no-x'], { from: 'user' });
+  expect(program.opts().y).toBe(false);
+});
+
+test('when boolean x implies y and -x, --no-y then y is false', () => {
+  const program = new commander.Command();
+  program
+    .option('-x')
+    .option('--no-y')
+    .implies('x', 'y');
+
+  program.parse(['-x', '--no-y'], { from: 'user' });
+  expect(program.opts().y).toBe(false);
+});
+
